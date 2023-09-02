f5ae786ac6306638232fae9790d87cf9c9a7d1a9
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-09-02 12:32:29 -0600

    Begin working on US2, set up show test for Recipe

---
+++ spec/features/recipes/show_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe Recipe, type: :feature do
+  it "As a visitor,
+  When I visit '/recipes/:id',
+  Then I see the recipe's name, complexity and genre,
+  and I see a list of the names of the ingredients for the recipe." do
+    
+  end
+end
