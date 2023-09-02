aeccd9aa9f8d716c5530325b7adc114ffae726da
Lane Bretschneider <lanebret85@gmail.com>
2023-09-02 16:27:36 -0700

    feat: add update action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,11 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
     @total_cost = @recipe.total_cost
   end
+
+  def update
+    ingredient = Ingredient.find(params[:ingredient_id])
+    Recipe.first.ingredients.create!(name: ingredient.name, cost: ingredient.cost)
+
+    redirect_to "/recipes/#{Recipe.first.id}"
+  end
 end
