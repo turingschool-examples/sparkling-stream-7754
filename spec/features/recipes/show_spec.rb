a5b9669609666e3d3d2421e3149e850a692aab77
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-09-02 13:53:16 -0600

    Make changes to make code more DRY and readable

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -1,20 +1,30 @@
 require "rails_helper"
 
 RSpec.describe Recipe, type: :feature do
+  before(:each) do 
+    @recipe = Recipe.create(name: "Classic Beef", complexity: "Easy", genre: "Quick and easy foods")
+    @ingredient_1 = @recipe.ingredients.create(name: "Salt", cost: 4)
+    @ingredient_2 = @recipe.ingredients.create(name: "Ground beef", cost: 2)
+  end
   it "As a visitor,
   When I visit '/recipes/:id',
   Then I see the recipe's name, complexity and genre,
   and I see a list of the names of the ingredients for the recipe." do
-    recipe = Recipe.create(name: "Classic Beef", complexity: "Easy", genre: "Quick and easy foods")
-    ingredient_1 = recipe.ingredients.create(name: "Salt", cost: 4)
-    ingredient_2 = recipe.ingredients.create(name: "Ground beef", cost: 2)
+    visit "/recipes/#{@recipe.id}"
 
-    visit "/recipes/#{recipe.id}"
+    expect(page).to have_content(@recipe.name)
+    expect(page).to have_content(@recipe.complexity)
+    expect(page).to have_content(@recipe.genre)
+    expect(page).to have_content(@ingredient_1.name)
+    expect(page).to have_content(@ingredient_2.name)
+  end
 
-    expect(page).to have_content(recipe.name)
-    expect(page).to have_content(recipe.complexity)
-    expect(page).to have_content(recipe.genre)
-    expect(page).to have_content(ingredient_1.name)
-    expect(page).to have_content(ingredient_2.name)
+  it "As a visitor,
+  When I visit '/recipes/:id'
+  I see the total cost of all of the ingredients in the recipe.
+  (e.g. Total Cost: 22)" do 
+    visit "/recipes/#{@recipe.id}"
+
+    expect(page).to have_content(@recipe.name)
   end
 end
