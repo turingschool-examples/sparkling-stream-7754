e17c5903119dba88a4e247bfe307591e367c8c68
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-09-02 12:09:05 -0600

    Begin testing US1

---
+++ spec/features/ingredients/index_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.describe Ingredient, type: :feature do
+  it "As a visitor,
+  When I visit '/ingredients'
+  I see a list of all the ingredients including their name and cost
+  (e.g. 'Ground Beef: 2' 'Salt: 4')"
+end
