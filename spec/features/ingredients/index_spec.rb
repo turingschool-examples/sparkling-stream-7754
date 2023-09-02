746a97c54114891d95649fb1677bb9a795a0f1af
Lane Bretschneider <lanebret85@gmail.com>
2023-09-02 15:30:28 -0700

    test: add test for ingredients appearing in alphabetical order

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -13,6 +13,21 @@ RSpec.describe "Ingredients Index Page" do
         expect(page).to have_content("#{ground_beef.name}: #{ground_beef.cost}")
         expect(page).to have_content("#{salt.name}: #{salt.cost}")
       end
+
+      it "displays the list of ingredients in alphabetical order by name" do
+        ground_beef = Ingredient.create!(name: "Ground Beef", cost: 2)
+        salt = Ingredient.create!(name: "Salt", cost: 4)
+        eggs = Ingredient.create!(name: "Eggs", cost: 3)
+        milk = Ingredient.create!(name: "Milk", cost: 2)
+        flour = Ingredient.create!(name: "Flour", cost: 5)
+
+        visit "/ingredients"
+
+        expect(eggs).to appear_before(flour)
+        expect(flour).to appear_before(ground_beef)
+        expect(ground_beef).to appear_before(milk)
+        expect(milk).to appear_before(salt)
+      end
     end
   end
 end
