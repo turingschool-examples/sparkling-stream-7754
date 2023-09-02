bed73b04f2a9c9bb5be4099e25d94bd896b50f0f
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-09-02 12:38:58 -0600

    Continue with show test

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe Recipe, type: :feature do
   When I visit '/recipes/:id',
   Then I see the recipe's name, complexity and genre,
   and I see a list of the names of the ingredients for the recipe." do
-    
+    ingredient_1 = Ingredient.create(name: "Ground Beef", cost: 2)
+    ingredient_2 = Ingredient.create(name: "Salt", cost: 4)
+    recipe = Recipe.create(name: "Classic Beef", complexity: "Minimal", genre: "Quick and easy foods")
+
+    recipe.ingredients << ingredient_1
+    recipe.ingredients << ingredient_2
   end
 end
