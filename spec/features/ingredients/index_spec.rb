f3ecc9bc57b69b37177914b2b58edb084779e33e
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-09-02 14:04:05 -0600

    Refactor test, begin creating extension

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -1,16 +1,23 @@
 require "rails_helper"
 
 RSpec.describe Ingredient, type: :feature do
+  before(:each) do 
+    @ingredient_1 = Ingredient.create(name: "Ground Beef", cost: 2)
+    @ingredient_2 = Ingredient.create(name: "Salt", cost: 4)
+  end
+
   it "As a visitor,
   When I visit '/ingredients'
   I see a list of all the ingredients including their name and cost
   (e.g. 'Ground Beef: 2' 'Salt: 4')" do 
-    ingredient_1 = Ingredient.create(name: "Ground Beef", cost: 2)
-    ingredient_2 = Ingredient.create(name: "Salt", cost: 4)
-
     visit "/ingredients"
 
     expect(page).to have_content("Ground Beef: 2")
     expect(page).to have_content("Salt: 4")
+  end
+  it "As a visitor,
+  When I visit '/ingredients',
+  Then I see that the list of ingredients is sorted alphabetically by name" do 
+
   end
 end
