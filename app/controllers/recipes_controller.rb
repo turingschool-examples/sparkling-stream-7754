3dc301e95b0a6f6e99dbd2a09268529898870ab6
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-09-02 13:24:11 -0600

    Make changes to follow MVC and pass the test

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,5 @@
 class RecipesController < ApplicationController
   def show 
     @recipe = Recipe.find(params[:id])
-    @ingredients = @recipe.ingredients.build
   end
 end
--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -5,15 +5,16 @@ RSpec.describe Recipe, type: :feature do
   When I visit '/recipes/:id',
   Then I see the recipe's name, complexity and genre,
   and I see a list of the names of the ingredients for the recipe." do
-    recipe = Recipe.create(name: "Classic Beef", complexity: "Minimal", genre: "Quick and easy foods")
-    recipe.ingredients.create(name: "Salt", cost: 4)
-    recipe.ingredients.create(name: "Ground beef", cost: 2)
+    recipe = Recipe.create(name: "Classic Beef", complexity: "Easy", genre: "Quick and easy foods")
+    ingredient_1 = recipe.ingredients.create(name: "Salt", cost: 4)
+    ingredient_2 = recipe.ingredients.create(name: "Ground beef", cost: 2)
 
     visit "/recipes/#{recipe.id}"
 
     expect(page).to have_content(recipe.name)
     expect(page).to have_content(recipe.complexity)
     expect(page).to have_content(recipe.genre)
-    expect(page).to have_content(recipe.ingredients.name)
+    expect(page).to have_content(ingredient_1.name)
+    expect(page).to have_content(ingredient_2.name)
   end
 end
