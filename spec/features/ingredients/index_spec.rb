27a6affeb9b5add2b9df3e60cc5043a2b9968dc5
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-09-02 12:13:36 -0600

    Create first feature test for index of ingredients

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -4,5 +4,13 @@ RSpec.describe Ingredient, type: :feature do
   it "As a visitor,
   When I visit '/ingredients'
   I see a list of all the ingredients including their name and cost
-  (e.g. 'Ground Beef: 2' 'Salt: 4')"
+  (e.g. 'Ground Beef: 2' 'Salt: 4')" do 
+    ingredient_1 = Ingredient.create(name: "Ground Beef", cost: 2)
+    ingredient_2 = Ingredient.create(name: "Salt", cost: 4)
+
+    visit "/ingredients"
+
+    expect(page).to have_content("Ground Beef: 2")
+    expect(page).to have_content("Salt: 4")
+  end
 end
