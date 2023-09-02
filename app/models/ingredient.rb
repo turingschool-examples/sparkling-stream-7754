ea00a28c3e66de5d0b873a1649174e007521a619
Lane Bretschneider <lanebret85@gmail.com>
2023-09-02 15:46:09 -0700

    feat: add sort_alpha method

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,10 @@
 class Ingredient < ApplicationRecord
    has_many :recipe_ingredients
    has_many :recipes, through: :recipe_ingredients
-   
+
    validates_presence_of :name, :cost
+
+   def self.sort_alpha
+      order(:name)
+   end
 end
