6f843f236ec83f567e318bf51576e069e0d05b82
Lane Bretschneider <lanebret85@gmail.com>
2023-09-02 15:05:36 -0700

    feat: add a recipe and its ingredients to the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 ground_beef = Ingredient.create!(name: "Ground Beef", cost: 2)
 salt = Ingredient.create!(name: "Salt", cost: 4)
+pancakes = Recipe.create!(name: "Pancakes", complexity: 1, genre: "Breakfast")
+eggs = pancakes.ingredients.create!(name: "Eggs", cost: 3)
+milk = pancakes.ingredients.create!(name: "Milk", cost: 2)
+flour = pancakes.ingredients.create!(name: "Flour", cost: 5)
