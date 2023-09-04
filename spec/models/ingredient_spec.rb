fa9f61732b62e6952f6fb787c889b9d12ceea11e
Dylan Timmons <dylanstimmons@icloud.com>
2023-09-04 16:52:34 -0600

    Add Extension 1 test and function

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe Ingredient, type: :model do
       it { should have_many(:recipes).through(:recipe_ingredients) }
    end
 
+   describe "sort alphabetically by name" do
+      it "returns ingredients sorted by name" do
+         ingredient_1 = Ingredient.create(name: 'Eggs', cost: 2)
+         ingredient_2 = Ingredient.create(name: 'Butter', cost: 4)
+         ingredient_3 = Ingredient.create(name: 'Chocolate Chips', cost: 5)
+
+         sorted_ingredients = Ingredient.sorted
+
+         expect(sorted_ingredients.first.name).to eq("Butter")
+         expect(sorted_ingredients.last.name).to eq("Eggs")
+         expect(sorted_ingredients[1].name).to eq("Chocolate Chips")
+      end
+   end
+
 end
