55803c6c549b1b024b1fb39280f5997f2bc9275f
Nick Spencer <strangebrew202@gmail.com>
2023-09-04 11:46:35 -0600

    Add: #find_cost and #find_ingredients model tests for Recipe model

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,7 +1,15 @@
 require "rails_helper"
 
 RSpec.describe Recipe, type: :model do
-   
+  before(:each) do
+    @onion = Ingredient.create!(name: "Onion", cost: 1)
+    @tomato = Ingredient.create!(name: "Tomato", cost: 1)
+    @egg = Ingredient.create!(name: "Egg", cost: 2)
+    @shakshuka = Recipe.create!(name: "Shakshuka", complexity: 2, genre: "Breakfast")
+    RecipeIngredient.create!(recipe_id: @shakshuka.id, ingredient_id: @tomato.id)
+        RecipeIngredient.create!(recipe_id: @shakshuka.id, ingredient_id: @onion.id)
+        RecipeIngredient.create!(recipe_id: @shakshuka.id, ingredient_id: @egg.id)
+  end
   describe "validations" do
     it {should validate_presence_of :name}
     it {should validate_presence_of :complexity}
@@ -11,5 +19,19 @@ RSpec.describe Recipe, type: :model do
   describe "relationships" do
     it {should have_many :recipe_ingredients}
     it {should have_many(:ingredients).through(:recipe_ingredients)}
+  end
+
+  describe "#find_ingredients" do
+    it "returns all ingredients for a recipe" do
+
+      expect(@shakshuka.find_ingredients).to eq([@onion, @tomato, @egg])
+    end
+  end
+
+  describe "#find_cost" do
+    it "returns cost of ingredients for given recipe" do
+
+      expect(@shakshuka.find_cost).to eq(4)
+    end
   end
 end
