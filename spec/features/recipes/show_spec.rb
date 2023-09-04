06676c4696ce0eec2866939f22908f5912e4112d
Dylan Timmons <dylanstimmons@icloud.com>
2023-09-04 16:42:46 -0600

    Add User Story 3 test and functionality

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -16,4 +16,15 @@ RSpec.describe "Recipes show page" do
         expect(page).to have_content(ingredient_2.name)
         expect(page).to have_content(ingredient_3.name)
     end
+
+    it "Shows the cost of the recipe" do
+        cookies = Recipe.create!(name: "Cookies", complexity: 2, genre: "Dessert")
+        ingredient_1 = cookies.ingredients.create(name: 'Eggs', cost: 2)
+        ingredient_2 = cookies.ingredients.create(name: 'Butter', cost: 4)
+        ingredient_3 = cookies.ingredients.create(name: 'Chocolate Chips', cost: 5)
+
+        visit "/recipes/#{cookies.id}"
+
+        expect(page).to have_content("Total Cost: 11")
+    end
 end
