aa4bd6090af3ba1c9d149b2200b052b4fa86570a
Cory Powell <coryrpowell@gmail.com>
2023-09-04 23:27:59 -0400

    added seed instances of recipe_ingredient model that would represent the relationship of a recipe with an ingredient

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+
+
 # recipe 1 should use 1,2,3,4 and recipe 2 should use 1,2,5
 @recipe_1 = Recipe.create!(name: "Spaghetti", complexity: 3, genre: "Italian")
 @recipe_2 = Recipe.create!(name: "Burger", complexity: 1, genre: "American")
@@ -15,4 +17,13 @@
 @ingredient_3 = Ingredient.create!(name: "Noodles", cost: 1)
 @ingredient_4 = Ingredient.create!(name: "Tomatoes", cost: 2)
 @ingredient_5 = Ingredient.create!(name: "Burg Bun", cost: 1)
+
+@spaghetti_1 = RecipeIngredient.create!(recipe_id: "#{@recipe_1.id}", ingredient_id: "#{@ingredient_1.id}")
+@spaghetti_2 = RecipeIngredient.create!(recipe_id: "#{@recipe_1.id}", ingredient_id: "#{@ingredient_2.id}")
+@spaghetti_3 = RecipeIngredient.create!(recipe_id: "#{@recipe_1.id}", ingredient_id: "#{@ingredient_3.id}")
+@spaghetti_4 = RecipeIngredient.create!(recipe_id: "#{@recipe_1.id}", ingredient_id: "#{@ingredient_4.id}")
+@burger_1 = RecipeIngredient.create!(recipe_id: "#{@recipe_2.id}", ingredient_id: "#{@ingredient_1.id}")
+@burger_2 = RecipeIngredient.create!(recipe_id: "#{@recipe_2.id}", ingredient_id: "#{@ingredient_2.id}")
+@burger_3 = RecipeIngredient.create!(recipe_id: "#{@recipe_2.id}", ingredient_id: "#{@ingredient_5.id}")
+
 
