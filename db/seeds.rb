5957de9e9d8b8c57665a6f39bb8ed5bd2ea5cf5d
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-09-05 16:52:28 -0500

    Add more examples to the test and add them to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+ingredient1 = Ingredient.create(name: "Apple", cost: 1)
+ingredient2 = Ingredient.create(name: "Pie Crust", cost: 2)
+ingredient3 = Ingredient.create(name: "Pie Filling", cost: 2)
--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -3,8 +3,10 @@ require "rails_helper"
 RSpec.describe "the Ingredients index page" do
    it "should show a list of all the ingredients including their name and cost" do
       ingredient1 = Ingredient.create(name: "Apple", cost: 1)
+      ingredient2 = Ingredient.create(name: "Pie Crust", cost: 2)
+      ingredient3 = Ingredient.create(name: "Pie Filling", cost: 2)
       visit "/ingredients"
-      
+
       expect(page).to have_content("Apple: 1")
    end
 end
