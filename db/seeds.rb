6ae1879dc947bdba02d3c4e672640127f6f25ac6
Cory Powell <coryrpowell@gmail.com>
2023-09-04 23:35:12 -0400

    added destroy_all method to seed file to attempt to fix testing error

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+Recipe.destroy_all
+Ingredient.destroy_all
+RecipeIngredient.destroy_all
 
 
 # recipe 1 should use 1,2,3,4 and recipe 2 should use 1,2,5
