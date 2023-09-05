00cd10962c5d3ad77aa1550e805bbd94d0d4282d
Will Weston <Williamweston81@gmail.com>
2023-09-05 18:13:51 -0500

    Weston(1) Test: Model test for recipe total cost

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe Recipe, type: :model do
     it {should have_many :recipe_ingredients}
     it {should have_many(:ingredients).through(:recipe_ingredients)}
   end
+
+  describe "total cost" do
+    it "can calculate the total cost of all ingredients" do
+      recipe_1 = Recipe.create!(name: "Burrito Bowl", complexity: 2, genre: "Latin")
+      ingredient_1 = Ingredient.create!(name: "Rice", cost: 4)
+      ingredient_2 = Ingredient.create!(name: "Avocado", cost: 1)
+      ingredient_3 = Ingredient.create!(name: "Chicken", cost: 7)
+      recipe_1.ingredients << ingredient_1
+      recipe_1.ingredients << ingredient_2
+      recipe_1.ingredients << ingredient_3
+
+      expect(recipe_1.total_cost).to eq(12)
+    end
+  end
 end
