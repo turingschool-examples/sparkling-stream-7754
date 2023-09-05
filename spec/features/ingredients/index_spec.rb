e75ab9c50ec4d987888bedf07e0f887af570b69d
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-09-05 16:35:17 -0500

    Create the first test for User story 1

---
+++ spec/features/ingredients/index_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.describe "the Ingredients index page" do
+   it "should show a list of all the ingredients including their name and cost" do
+      ingredient1 = Ingredient.create(name: "Apple", cost: 1)
+      expect(page).to have_content("Apple: 1")
+   end
+end
