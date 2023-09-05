3142e8c490f9536684b9483ca7134d44e0482063
Cory Powell <coryrpowell@gmail.com>
2023-09-04 21:50:16 -0400

    added more seeds for the recipe model and quick ref for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,13 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+# recipe 1 should use 1,2,3,4 and recipe 2 should use 1,2,5
+@recipe_1 = Recipe.create!(name: "Spaghetti", complexity: 3, genre: "Italian")
+@recipe_2 = Recipe.create!(name: "Burger", complexity: 1, genre: "American")
+
 @ingredient_1 = Ingredient.create!(name: "Ground Beef", cost: 2)
 @ingredient_2 = Ingredient.create!(name: "Salt", cost: 4)
+@ingredient_3 = Ingredient.create!(name: "Noodles", cost: 1)
+@ingredient_4 = Ingredient.create!(name: "Tomatoes", cost: 2)
+@ingredient_5 = Ingredient.create!(name: "Burg Bun", cost: 1)
+
