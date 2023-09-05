605a3a33b2f4aacdd58148819cd60c287fb5d431
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-09-05 18:20:30 -0500

    add test for recipes show

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,4 +2,8 @@ class IngredientsController < ApplicationController
   def index
     @ingredients = Ingredient.all
   end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
 end
--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -1,30 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "Recipes Show" type: :feature do
+RSpec.describe "Recipes Show" do
   it "Display a recipe's name, complexity and genre, and I see a list of the names of the ingredients for the recipe." do
 
-    @recipe_1 = Recipe.create!( name: "Taco Meat", complexity: 10, genre: "Mexican")
+    @recipe = Recipe.create!( name: "Taco Meat", complexity: 10, genre: "Mexican")
     @ingredient_1 = Ingredient.create!(name: "Ground Beef", cost: 2)
     @ingredient_2 = Ingredient.create!(name: "Salt", cost: 4)
    
-    @recipe_1.ingredients << [@ingredient_1, @ingredient_2]
+    @recipe.ingredients << [@ingredient_1, @ingredient_2]
 
-    visit "/recipes/#{recipe.id}"
+    visit "/recipes/#{@recipe.id}"
 
-    expect(page).to have_content(@recipe_1.name)
-    expect(page).to have_content(@recipe_1.complexity)
-    expect(page).to have_content(@recipe_1.genre)
+    expect(page).to have_content(@recipe.name)
+    expect(page).to have_content(@recipe.complexity)
+    expect(page).to have_content(@recipe.genre)
 
 
     expect(page).to have_content("List of Ingredients for recipe")
-    expect(page).to have_content("Ground Beef")
-    expect(page).to have_content("Salt")
+    expect(page).to have_content(@ingredient_1.name)
+    expect(page).to have_content(@ingredient_2.name)
   end
 end
-
-User Story 2 - Recipes Show
-
-As a visitor,
-When I visit '/recipes/:id',
-Then I see the recipe's name, complexity and genre,
-and I see a list of the names of the ingredients for the recipe.
