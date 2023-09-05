e4ec8f77688db87a8543b26e3b47b84f295566e8
Nicholas McEnroe <nsmcenro@gmail.com>
2023-09-05 06:34:40 -0600

    Test: Add US 3 test

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -7,12 +7,22 @@ RSpec.describe "recipe show page" do
     ingredient_2 = recipe.ingredients.create(name: "Salt", cost: 4)
 
     visit "/recipes/#{recipe.id}"
-    save_and_open_page
 
     expect(page).to have_content(recipe.name)
     expect(page).to have_content(recipe.complexity)
     expect(page).to have_content(recipe.genre)
     expect(page).to have_content(ingredient_1.name)
     expect(page).to have_content(ingredient_2.name)
+  end
+
+  it "displays the total cost of the ingredients" do
+    recipe = Recipe.create(name: "Salted Grounded Beef", complexity: 1, genre: "meat")
+    ingredient_1 = recipe.ingredients.create(name: "Ground Beef", cost: 2)
+    ingredient_2 = recipe.ingredients.create(name: "Salt", cost: 4)
+
+    visit "/recipes/#{recipe.id}"
+
+    expect(page).to have_content(6)
+
   end
 end
