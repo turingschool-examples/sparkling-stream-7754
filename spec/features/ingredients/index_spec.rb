776d5fe28004ef1ea318019cc1b47dd945c120d1
Nicholas McEnroe <nsmcenro@gmail.com>
2023-09-05 06:32:25 -0600

    Feat: Add US 2

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe 'ingredients index' do
     ingredient_2 = Ingredient.create(name: "Salt", cost: 4)
 
     visit "/ingredients"
-    save_and_open_page
 
     expect(page).to have_content(ingredient_1.name)
     expect(page).to have_content(ingredient_1.cost)
--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -2,9 +2,12 @@ require "rails_helper"
 
 RSpec.describe "recipe show page" do
   it "displays the recipe's name, complexity, and genre, along with all of its ingredients" do
-    recipe = Recipe.create(name: "Salted Grounded Beef", complexity: "easy", genre: "meat")
+    recipe = Recipe.create(name: "Salted Grounded Beef", complexity: 1, genre: "meat")
     ingredient_1 = recipe.ingredients.create(name: "Ground Beef", cost: 2)
     ingredient_2 = recipe.ingredients.create(name: "Salt", cost: 4)
+
+    visit "/recipes/#{recipe.id}"
+    save_and_open_page
 
     expect(page).to have_content(recipe.name)
     expect(page).to have_content(recipe.complexity)
