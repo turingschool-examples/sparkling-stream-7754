5a0cf95eca870da4db3057dc98b3a175a6f4e334
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-09-04 21:02:27 -0600

    set up the tests for the recipe show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-ingredient1 = Ingredient.create!(name: "Ground Beef", cost: "2")
-ingredient2 = Ingredient.create!(name: "Salt", cost: "1")
-ingredient3 = Ingredient.create!(name: "Rice", cost: "4")
+@recipe_1 = Recipe.create!(name: "Pasta", complexity: 1, genre: "Italian")
+@recipe_2 = Recipe.create!(name: "Beef Bowl", complexity: 2, genre: "Asian")
+@recipe_3 = Recipe.create!(name: "Burrito Bowl", complexity: 2, genre: "Mexican")
+
+@ingredient_1 = @recipe_1.ingredients.create!(name: "Ground Beef", cost: 2)
+@ingredient_2 = @recipe_1.ingredients.create!(name: "Noodles", cost: 1)
+@ingredient_3 = @recipe_1.ingredients.create!(name: "Tomato Sauce", cost: 4)
+
+@ingredient_1 = @recipe_2.ingredients.create!(name: "Ground Beef", cost: 2)
+@ingredient_4 = @recipe_2.ingredients.create!(name: "Rice", cost: 4)
+@ingredient_5 = @recipe_2.ingredients.create!(name: "Broccoli", cost: 3)
+
+@ingredient_4 = @recipe_3.ingredients.create!(name: "Rice", cost: 4)
+@ingredient_1 = @recipe_3.ingredients.create!(name: "Ground Beef", cost: 2)
+@ingredient_6 = @recipe_3.ingredients.create!(name: "Bell Peppers", cost: 4)
---
+++ spec/features/recipes/show_spec.rb
@@ -0,0 +1,59 @@
+require "rails_helper"
+
+RSpec.describe "Recipe Show Page" do 
+  before(:each) do 
+    @recipe_1 = Recipe.create!(name: "Pasta", complexity: 1, genre: "Italian")
+    @recipe_2 = Recipe.create!(name: "Beef Bowl", complexity: 2, genre: "Asian")
+    @recipe_3 = Recipe.create!(name: "Burrito Bowl", complexity: 2, genre: "Mexican")
+
+    @ingredient_1 = @recipe_1.ingredients.create!(name: "Ground Beef", cost: 2)
+    @ingredient_2 = @recipe_1.ingredients.create!(name: "Noodles", cost: 1)
+    @ingredient_3 = @recipe_1.ingredients.create!(name: "Tomato Sauce", cost: 4)
+    
+    @ingredient_1 = @recipe_2.ingredients.create!(name: "Ground Beef", cost: 2)
+    @ingredient_4 = @recipe_2.ingredients.create!(name: "Rice", cost: 4)
+    @ingredient_5 = @recipe_2.ingredients.create!(name: "Broccoli", cost: 3)
+    
+    @ingredient_4 = @recipe_3.ingredients.create!(name: "Rice", cost: 4)
+    @ingredient_1 = @recipe_3.ingredients.create!(name: "Ground Beef", cost: 2)
+    @ingredient_6 = @recipe_3.ingredients.create!(name: "Bell Peppers", cost: 4)
+  end
+
+  it "can show the recipe name, complexity, and genre" do 
+    visit "/recipes/#{@recipe_1.id}"
+
+    expect(page).to have_content("Recipe Name: Pasta")
+    expect(page).to have_content("Complexity: 1")
+    expect(page).to have_content("Genre: Italian")
+    
+    visit "/recipes/#{@recipe_2.id}"
+    expect(page).to have_content("Recipe Name: Beef Bowl")
+    expect(page).to have_content("Complexity: 2")
+    expect(page).to have_content("Genre: Asian")
+    
+    visit "/recipes/#{@recipe_3.id}"
+    expect(page).to have_content("Recipe Name: Burrito Bowl")
+    expect(page).to have_content("Complexity: 2")
+    expect(page).to have_content("Genre: Mexican")
+  end
+  
+  it "can show a list of ingredients associated with that recipe" do 
+    visit "/recipes/#{@recipe_1.id}"
+
+    expect(page).to have_content("Ground Beef")
+    expect(page).to have_content("Noodles")
+    expect(page).to have_content("Tomato Sauce")
+    
+    visit "/recipes/#{@recipe_2.id}"
+    expect(page).to have_content("Ground Beef")
+    expect(page).to have_content("Rice")
+    expect(page).to have_content("Broccoli")
+
+    
+    visit "/recipes/#{@recipe_3.id}"
+    expect(page).to have_content("Ground Beef")
+    expect(page).to have_content("Rice")
+    expect(page).to have_content("Bell Peppers")
+  end
+        
+end
