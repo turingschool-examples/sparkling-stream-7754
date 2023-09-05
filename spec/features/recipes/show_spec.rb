69c9df9bd755fa55e155fa45b33a532e14996d8f
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-09-05 17:39:00 -0500

    update testing for recipes to show a given recipe and its ingredients

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -1,3 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "Recipes Show" type: :feature do
+  it "Display a recipe's name, complexity and genre, and I see a list of the names of the ingredients for the recipe." do
+
+    @recipe_1 = Recipe.create!( name: "Taco Meat", complexity: 10, genre: "Mexican")
+    @ingredient_1 = Ingredient.create!(name: "Ground Beef", cost: 2)
+    @ingredient_2 = Ingredient.create!(name: "Salt", cost: 4)
+   
+    @recipe_1.ingredients << [@ingredient_1, @ingredient_2]
+
+    visit "/recipes/#{recipe.id}"
+
+    expect(page).to have_content(@recipe_1.name)
+    expect(page).to have_content(@recipe_1.complexity)
+    expect(page).to have_content(@recipe_1.genre)
+
+
+    expect(page).to have_content("List of Ingredients for recipe")
+    expect(page).to have_content("Ground Beef")
+    expect(page).to have_content("Salt")
+  end
+end
+
 User Story 2 - Recipes Show
 
 As a visitor,
