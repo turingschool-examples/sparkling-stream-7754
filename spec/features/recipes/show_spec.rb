ace91cf57cd02d8d18cff5257c491e97ade3f5cf
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-09-05 17:21:20 -0500

    create the recipes/show_spec test file

---
+++ spec/features/recipes/show_spec.rb
@@ -0,0 +1,6 @@
+User Story 2 - Recipes Show
+
+As a visitor,
+When I visit '/recipes/:id',
+Then I see the recipe's name, complexity and genre,
+and I see a list of the names of the ingredients for the recipe.
